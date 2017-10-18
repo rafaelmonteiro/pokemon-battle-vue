d0a34873e7f75b27fcf2336653bfb0f0497ba1ef
Rafael Monteiro <rafaelmonteiro@santos.sp.gov.br>
2017-10-18 09:51:58 -0200

    Information about gaming calculations

--- src/components/header/help/index.vue
+++ src/components/header/help/index.vue
@@ -6,10 +6,19 @@
                     <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
                         <span aria-hidden="true">&times;</span>
                     </button>
-                    <h4 class="modal-title">About the Pokemon Battle</h4>
+                    <h4 class="modal-title">About Pokémon Battle</h4>
                 </div>
                 <div class="modal-body">
-                    <p>Info</p>
+                    <p>This game considers the traditional Pokémon table of advantages/disadvantages. </br>
+                    For instance, Electric attacks causes double damage over Water Pokémons but only half
+                    on a Grass type. 
+                    </p>
+                    <p>
+                    Also, each attack has a probability to fail (miss the target) or cause critical damage (double damage).
+                    Basically, this calculation considers attacker's strength, target's defense, a multiplier regarding 
+                    types and a random value.
+                    </p>
+                    <p>Have fun!</p>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-primary" data-dismiss="modal">
